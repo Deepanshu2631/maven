ff27279f34e8dd452b0f6d96ad7f8889d637c70d
Ajfsamd39 <Ajfsamd39@Ajfsamd11.iiht.tech>
2022-06-29 10:40:04 +0530

    updated

--- src/Java/MyJava.java
+++ src/Java/MyJava.java
@@ -3,8 +3,11 @@ package Java;
 public class MyJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hello hhii");
+		// TODO Auto-generated method
+ System.out.println("hello hlo");
+            System.out.println("hello hlo");
+	
+	
 	}
 
 }
